4dde27821be38156608eb1f42754b9ff14171969
Paul Burt <paul.burt@gmail.com>
2012-05-23 12:07:14 +0100

    Updated akvo.rsr.views.api.JsonResponse to hardcode content_type as "application/json".

--- akvo/rsr/views/api/__init__.py
+++ akvo/rsr/views/api/__init__.py
@@ -3,9 +3,9 @@ import simplejson as json
 
 
 class JsonResponse(HttpResponse):
-    """django.http.HttpResponse object which accepts a native Python object
+    """django.http.HttpResponse object which accepts a native Python data structure
     and returns an HTTP response with JSON body and appropriate headers.
     """
-    def __init__(self, content, content_type="application/json", **kwargs):
+    def __init__(self, content, **kwargs):
         self.content = json.dumps(content)
-        self.content_type = content_type
+        self.content_type = "application/json"
